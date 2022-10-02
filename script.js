01fdaebdc26d4edc5c4fa6a8c7b8f3dc33b6eefe
Charamest <dgorynyc747@gmail.com>
2022-10-02 18:04:32 +0300

    добавил модальное окно

--- script.js
+++ script.js
@@ -4,24 +4,42 @@ const modal = document.getElementById("myModal")
 const openBtn = document.getElementById("myBtn")
 const closeBtn = document.getElementById("close")
 
-openBtn.onclick = function (){
-    modal.style.display = "block" ;
+openBtn.onclick = function () {
+    modal.style.display = "block";
 }
-closeBtn.onclick = function (){
-    modal.style.display = "none" ;
+closeBtn.onclick = function () {
+    modal.style.display = "none";
 
 }
-modal.onclick = function (event ) {
-   if (event.target == modal){
-    modal.style.display = "none"
-   }
+modal.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
 }
+
 // function gift(){
 //     const name=prompt("Введите имя")
 //     alert("Поздравляем, " + name + "! Вы получили подарок! Скидка 25%!")
 //     console.log("Подарок")
 // }
 
-function print_contact(){
-    alert(" skype:helper \n tel:79856432545")
+const modalhelp = document.getElementById("helperModal")
+const helpBtn = document.getElementById("helperBtn")
+const closehelpBtn = document.getElementById("closerBtn")
+
+helpBtn.onclick = function () {
+    modalhelp.style.display = "block";
 }
+closehelpBtn.onclick = function () {
+    modalhelp.style.display = "none";
+
+}
+modalhelp.onclick = function (event) {
+    if (event.target == modalhelp) {
+        modalhelp.style.display = "none"
+    }
+}
+
+// function print_contact(){
+//     alert(" skype:helper \n tel:79856432545")
+// }
