65ece6382187a29d94cc6b28018026c942fd2180
Charamest <dgorynyc747@gmail.com>
2022-10-10 11:43:18 +0300

    Оценивание

--- script.js
+++ script.js
@@ -43,3 +43,68 @@ modalhelp.onclick = function (event) {
 // function print_contact(){
 //     alert(" skype:helper \n tel:79856432545")
 // }
+let siteRating = 0;
+function star(starNumber) {
+    siteRating = starNumber;
+    starNumber = starNumber - 1;
+    // console.log(starNumber)
+    let ids = ["star-1", "star-2", "star-3", "star-4", "star-5"];
+    // console.log(ids[starNumber]);
+    let star = document.getElementById(ids[starNumber]);
+    // console.log(star);
+    if (star.src.includes("images/star_2.png")) {
+        for (let i = 0; i <= starNumber; i++) {
+            let id = ids[i];
+            document.getElementById(id).src = "images/star_1.png";
+        }
+    }
+
+    else {
+        for (let i = 0; i < ids.length; i++) {
+            if (i > starNumber) {
+                id = ids[i];
+                document.getElementById(id).src = "images/star_2.png";
+            }
+        }
+    }
+
+}
+function resetRating() {
+    //  console.log('Сбросить рейтинг')
+    siteRating = 0
+    let ids = ["star-1", "star-2", "star-3", "star-4", "star-5"];
+    for (let i = 0; i < ids.length; i++) {
+        let id = ids[i];
+        document.getElementById(id).src = "images/star_2.png";
+    }
+}
+
+const sandRatingBtn = document.getElementById("sendRatingBtn")
+
+sandRatingBtn.addEventListener("click", function () {
+    // console.log("Отправить рейтинг")
+    let text;
+    // if (siteRating == 5); {
+    //     text = "Спасибо за высокую оценку"
+    // }
+    // if (siteRating == 4); {
+    //     text = "Спасибо за хороший бал"
+    // }
+    switch (siteRating) {
+        case 1:
+        case 2:
+        case 3:
+            text = "Спасибо за ваш бал!"
+            break;
+        case 4:
+            text = "Спасибо за хороший бал!"
+            break;
+        case 5:
+            text = "Спасибо за выший бал!"
+            break;
+        default:
+            text = "Пожалуйста, поставьте оценку от 1 до 5";
+    }
+    document.getElementById('ratingText').innerText = text;
+
+});
